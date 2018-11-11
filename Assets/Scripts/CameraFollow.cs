74f4fab95e0f73103dee83361540f967aeb46fa2
Cyrusc <cyruscmartien@gmail.com>
2018-11-10 23:02:19 -0600

    Added scriptable room changes

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -11,13 +11,4 @@ public class CameraFollow : MonoBehaviour {
         smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
     }
-
-    private void OnDrawGizmos() {
-        Gizmos.color = Color.black;
-        Vector3 offset = new Vector3(0, 0, 1);
-        Vector3 topMiddle = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height, 0)) + offset;
-        Vector3 bottomMiddle = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, 0, 0)) + offset;
-        
-        Gizmos.DrawLine(topMiddle, bottomMiddle);
-    }
 }
