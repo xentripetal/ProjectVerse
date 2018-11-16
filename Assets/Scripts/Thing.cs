40b08ea5ccb36e22a8b6272b1855b2a6cec9329b
cyrusc <cyruscmartien@gmail.com>
2018-11-15 23:55:56 -0600

    Reorganized

--- Assets/Scripts/Thing.cs
+++ Assets/Scripts/Thing.cs
@@ -13,8 +13,10 @@ public class Thing {
         private set;
     }
 
-   public Thing(String name, String resourcePath) {
+   public Thing(String name, String resourcePath, ScriptableObject) {
        this.name = name;
        sprite = Resources.Load<Sprite>(resourcePath);
    }
+    
+    
 }
---
+++ Assets/Scripts/ThingScripts/DoorTriggerData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTriggerData : MonoBehaviour {
+    public string room;
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        FindObjectOfType<Room>().changeRoom(room);
+    }
+}
