357f27646ea39060e8f43987a07650bfa66fca16
cyrusc <cyruscmartien@gmail.com>
2018-11-29 16:29:45 -0600

    Started standardizing JSON models

---
+++ Assets/Scripts/Models/JSONModels/TerrainMap.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using API.Models;
+using Newtonsoft.Json;
+
+public class SerializableTile{
+    public string Definition;
+    public Position Position;
+}
+
+public class BoxCollider {
+    public Position Position;
+    public Position Size;
+}
+
+public class Colliders {
+    public IList<Position> EdgePoints;
+    public IList<BoxCollider> BoxColliders;
+
+    public Colliders(IList<Position> edgePoints, IList<BoxCollider> boxColliders) {
+        EdgePoints = edgePoints;
+        BoxColliders = boxColliders;
+    }
+}
+
+[JsonObject(MemberSerialization.OptIn)]
+public class TerrainMap {
+    public Colliders Colliders;
+    public IList<SerializableTile> Tiles;
+    
+    public TerrainMap(Colliders colliders, IList<SerializableTile> tiles) {
+        Colliders = colliders;
+        Tiles = tiles;
+    }
+}
